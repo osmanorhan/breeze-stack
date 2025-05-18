9d9b1bd02d7912be1919d64239e63e7c8a918f18
Osman Orhan <osman.orhan@gmail.com>
2025-05-18 15:45:39 +0300

    chore: bump versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path"
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app"),
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
 });
